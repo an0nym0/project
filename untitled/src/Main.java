5b73addfa44b51726ff2148d312b9db28e9bed74
an0num0 <ivanov.ivan.s.1987@gmail.com>
2018-05-22 15:35:09 +0600

    m3

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("master");
+        System.out.println("master");
+        System.out.println("m2");
+        System.out.println("m3");
+    }
 }
