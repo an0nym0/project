e5803d65b06f8025671c405876017d9ca8cb4f77
an0num0 <ivanov.ivan.s.1987@gmail.com>
2018-05-22 15:40:34 +0600

    b2

---
+++ untitled/src/B.java
@@ -0,0 +1,7 @@
+public class B {
+    public static void main(String[] args) {
+        System.out.println("5");
+        System.out.println("6");
+        System.out.println("7");
+        System.out.println("8");
+}
