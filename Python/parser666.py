d4ea865ab471ca8152b4f805067749c5ddd7a885
an0num0 <ivanov.ivan.s.1987@gmail.com>
2018-10-22 11:04:58 +0600

    master

--- Python/parser666.py
+++ Python/parser666.py
@@ -1,8 +1,9 @@
 import re
 
+
 def main():
-    in_f = open('E:/Projects/parse_emails_and_other_stuff/export5/export5.csv',"r")
-    out_f = open('E:/Projects/parse_emails_and_other_stuff/export5/export6.csv',"w")
+    in_f = open('E:/Projects/parse_emails_and_other_stuff/export5/export5.csv', "r")
+    out_f = open('E:/Projects/parse_emails_and_other_stuff/export5/export6.csv', "w")
     i = 0
 
     for l in in_f.readlines():
@@ -10,26 +11,24 @@ def main():
         string = l.split(";")
         #		print string  ### TODO delete debug
 
-        i = i+1
+        i = i + 1
         string[10] = string[10][:-1]
         #		raw_input("Press Enter to continue...")  ### TODO delete debug
-        if i == 1 :
-            out_f.write(";".join(string)+"\n")
+        if i == 1:
+            out_f.write(";".join(string) + "\n")
             continue
 
-
         if not (re.match(r'\A[A-Z0-9-]{36}\Z', string[0])):
-            print "\nDied in string " + str(i) + " arg 1: \n" + l
+            print("\nDied in string " + str(i) + " arg 1: \n" + l)
             exit(1)
         elif not (re.match(r'\A[ ,A-Za-z-()]*\Z', string[4])):
-            print "\nDied in string " + str(i) + " arg 5: " + string[4] +"\n" + l
+            print("\nDied in string " + str(i) + " arg 5: " + string[4] + "\n" + l)
             exit(1)
         #		elif not re.search('[A-Z]|\d|-{22}', string[10] ):
         #			print "\nDied in string " + str(i) + " arg 10: \n" + l
         #			exit(1)
         else:
-            out_f.write(";".join(string)+"\n")
-
+            out_f.write(";".join(string) + "\n")
 
 
 if __name__ == "__main__":
